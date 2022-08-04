3cfbf56503c67a585eeff119cec75a40d07f65a6
shankar <sbshinde145@gmail.com>
2022-08-04 13:07:22 +0530

    dj_static2

--- assignment/settings.py
+++ assignment/settings.py
@@ -119,11 +119,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 
+
+STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,  "static"),
 ]
-STATIC_ROOT = 'staticfiles'
-STATIC_URL = '/static/'
 MESSAGE_TAGS = {
     messages.ERROR:'danger'
 }
